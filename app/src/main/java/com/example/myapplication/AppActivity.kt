00cd25707a7540c3610e23b875f5c38b966797e5
Kadun Vitaly <samtel58@yandex.ru>
2021-04-02 23:28:21 +0300

    15_10 Create all page in viewPager2

--- app/src/main/java/com/example/myapplication/AppActivity.kt
+++ app/src/main/java/com/example/myapplication/AppActivity.kt
@@ -7,29 +7,29 @@ import com.example.myapplication.databinding.ActivityAppBinding
 class AppActivity : AppCompatActivity() {
     private val articles: List<ArticleModel> = listOf(
         ArticleModel(
+            R.string.oboe_title,
+            R.string.oboe_text,
+            R.drawable.android_high_performance_game_audio_with_oboe_header
+        ), ArticleModel(
             R.string.android_12_title,
             R.string.android_12_text,
             R.drawable.android_12_logo
         ), ArticleModel(
-            R.string.android_12_title,
-            R.string.android_12_text,
-            R.drawable.android_12_logo
+            R.string.final_challenge_title,
+            R.string.final_chellenge_text,
+            R.drawable.final_challeng
         ), ArticleModel(
-            R.string.oboe_title,
-            R.string.oboe_text,
-            R.drawable.android_high_performance_game_audio_with_oboe_header
+            R.string.boost_develop_title,
+            R.string.boost_develop_text,
+            R.drawable.play_logo
         ), ArticleModel(
-            R.string.oboe_title,
-            R.string.oboe_text,
-            R.drawable.android_high_performance_game_audio_with_oboe_header
+            R.string.week_three_challenge_title,
+            R.string.week_three_challenge_text,
+            R.drawable.week_three_challenge
         ), ArticleModel(
-            R.string.oboe_title,
-            R.string.oboe_text,
-            R.drawable.android_high_performance_game_audio_with_oboe_header
-        ), ArticleModel(
-            R.string.oboe_title,
-            R.string.oboe_text,
-            R.drawable.android_high_performance_game_audio_with_oboe_header
+            R.string.sub_dollar_title,
+            R.string.sub_dollar_text,
+            R.drawable.sub_dollar
         )
     )
 
