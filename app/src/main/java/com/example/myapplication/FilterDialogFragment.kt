eec1643eac189c23a38e6851cf67b2445d7b1df0
Kadun Vitaly <samtel58@yandex.ru>
2021-04-27 22:51:50 +0300

    16_8 setup save state companyList and added TextView for empty screen

--- app/src/main/java/com/example/myapplication/FilterDialogFragment.kt
+++ app/src/main/java/com/example/myapplication/FilterDialogFragment.kt
@@ -4,11 +4,7 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 
 class FilterDialogFragment() : DialogFragment() {
     var booleanTags = booleanArrayOf(true, true, true, true)
